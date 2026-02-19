ff373f3f74761ec9c93885b5e1aa00ee75685ee6
Michael <miketraynor77@hotmail.com>
2026-02-19 13:37:39 +0000

    Got to commit here

--- 05-example-web-application/api-node/src/index.js
+++ 05-example-web-application/api-node/src/index.js
@@ -12,7 +12,7 @@ app.use(morgan('tiny'));
 app.get('/', async (req, res) => {
   const dateTime = await getDateTime();
   const response = dateTime;
-  response.api = 'node-mike-1';
+  response.api = 'node-mike-2';
   res.send(response);
 });
 
